6a47860889060a73da5be7eaeb7578cfbedb973b
Fraser Mince <fraser@thoughtbot.com>
2020-04-17 14:53:42 -0500

    WIP tinder clone.

--- src/TabNavigator.js
+++ src/TabNavigator.js
@@ -6,6 +6,7 @@ import { reactComponent as AccountScreen} from "../output/Account";
 import { reactComponent as DailySelections} from "../output/FlashcardBuilder.DailySelections";
 import { reactComponent as SentenceChoice} from "../output/FlashcardBuilder.SentenceChoice";
 import { reactComponent as ImageChoice} from "../output/FlashcardBuilder.ImageChoice";
+import Review from "./Tinder/Review"
 import { reactComponent as WordSelection} from "../output/FlashcardBuilder.WordSelection";
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import CommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
@@ -29,11 +30,13 @@ export default TabNavigator = () => {
   let bookIcon = ({focused, color}) => <Icon style={[{color: color}]} name={"book"} size={25} />
   let accountIcon = ({focused, color}) => <Icon style={[{color: color}]} name={"account-box"} size={25} />
   let flashcardIcon = ({focused, color}) => <CommunityIcon style={[{color: color}]} name={"card-bulleted-outline"} size={25} />
+  let reviewIcon = ({focused, color}) => <Icon style={[{color: color}]} name={"layers"} size={25} />
   return (
     <NavigationContainer>
       <Tab.Navigator barStyle={{backgroundColor: "black"}}>
         <Tab.Screen name="Home" component={IndexScreen} options={{tabBarIcon:  homeIcon}} />
         <Tab.Screen name="Flashcards" component={FlashcardNavigator} options={{tabBarIcon: flashcardIcon}} />
+        <Tab.Screen name="Review" component={Review} options={{tabBarIcon: reviewIcon}} />
         <Tab.Screen name="Read" component={BookScreen} options={{tabBarIcon:  bookIcon}} />
         <Tab.Screen name="Account" component={AccountScreen} options={{tabBarIcon:  accountIcon}} />
       </Tab.Navigator>
