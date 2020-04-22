53b6dd28351c821a525114a0fa23d878551ed0cb
Fraser Mince <fraser@thoughtbot.com>
2020-04-22 11:07:39 -0500

    Add logic to stop playing sounds.

--- src/FlashcardReview/CardItem.purs
+++ src/FlashcardReview/CardItem.purs
@@ -27,7 +27,7 @@ import Data.Maybe (Maybe(..), fromMaybe, isJust)
 import Effect.Class (liftEffect)
 import FS (audioDir, writeFile, exists, unlink, absintheFile)
 import Debug.Trace (spy)
-import Sound (play)
+import Sound (play, Sound, release, stop, createSound)
 
 gray = "#757E90"
 containerCardItem = {
@@ -69,7 +69,7 @@ descriptionCardItem = {
   fontSize: 20
 }
 
-type Props = {word :: String, sentence :: String, offset :: Int, imageUrl :: Array String, onPressLeft :: Effect Unit, onPressRight :: Effect Unit, index :: Int, audioUrl :: String, sentenceId :: String, setIsFlipped :: (Boolean -> Boolean) -> Effect Unit}
+type Props = {activeIndex :: Int, word :: String, sentence :: String, offset :: Int, imageUrl :: Array String, onPressLeft :: Effect Unit, onPressRight :: Effect Unit, index :: Int, audioUrl :: String, sentenceId :: String, setIsFlipped :: (Boolean -> Boolean) -> Effect Unit}
 
 imageJsx imageCount accum imageUrl = accum <> image {style: M.css $ imageStyle $ toNumber imageCount, source: {uri: imageUrl}}
 
@@ -83,25 +83,33 @@ fileExists :: Maybe String -> Aff Boolean
 fileExists (Just filePath) = exists filePath
 fileExists Nothing = pure false
 
-flipEnd setIsFlipped audioPath = do
+flipEnd setIsFlipped sound = do
   setIsFlipped \_ -> true
-  play $ fromMaybe "" audioPath
+  traverse_ play sound
 
 buildJsx props = React.do
   audioPath /\ setAudioPath <- useState (Nothing :: Maybe String)
+  sound /\ setSound <- useState (Nothing :: Maybe Sound)
+  let setAudioInformation path = do
+        setAudioPath \_ -> Just path
+        setSound \_ -> Just $ createSound path
+  useEffect (spy "ACTIVE" props.activeIndex) do
+     traverse_ (\s -> stop s mempty) sound
+     pure mempty
   useEffect (spy "PROPS" props).audioUrl do
      launchAff_ do
         let file = audioDir <> "/sentence-" <> props.sentenceId <> ".mp3"
         e <- exists file
         case e of
-          true -> liftEffect $ setAudioPath \_ -> Just $ "file://" <> file
+          true -> liftEffect $ setAudioInformation $ "file://" <> file
           false -> do
             result <- fetch {fileCache: true, path: spy "FILE" file} "GET" props.audioUrl {}
             path <- liftEffect $ spy "PATH" result.path
-            liftEffect $ setAudioPath \_ -> Just $ "file://" <> path
+            liftEffect $ setAudioInformation $ "file://" <> path
      pure $ launchAff_ do
         e <- fileExists audioPath
         if e then unlink $ fromMaybe "" audioPath else mempty
+        liftEffect $ traverse_ release sound 
 
 
   flipRef <- useRef null
@@ -109,9 +117,8 @@ buildJsx props = React.do
         result <- readRefMaybe flipRef
         traverse_ (\s -> bindThis s.flipY s) result
 
-
   pure $ M.getJsx do
-     card {index: props.index, style: M.css {width: window.width}, ref: flipRef, onFlipEnd: flipEnd props.setIsFlipped audioPath} do
+     card {index: props.index, style: M.css {width: window.width}, ref: flipRef, onFlipEnd: flipEnd props.setIsFlipped sound} do
         M.touchableOpacity {style: M.css containerCardItem, onPress: RNE.capture_ $ flip} do
             M.text {style: M.css promptStyle} $ M.string "What word goes in the blank"
             M.text {style: M.css {flexWrap:"wrap", flexDirection: "row", marginRight: 5, marginLeft: 5, flex: 4}} do
@@ -124,7 +131,7 @@ buildJsx props = React.do
             M.text {style: M.css {marginRight: 5, marginLeft: 5, flex: 2}} do
                underlineWordMarkup props.sentence props.offset props.word (M.css descriptionCardItem) "normal" 20
             M.view {style: M.css {flex: 3}} do
-              fab {icon: "volume-medium", small: true, style: M.css {width: 40}, onPress: RNE.capture_ $ play $ fromMaybe "" audioPath}
+              fab {icon: "volume-medium", small: true, style: M.css {width: 40}, onPress: RNE.capture_ $ traverse_ play sound}
             M.view {style: M.css {flexDirection: "column", flex: 1, width: window.width - 60.0, justifyContent: "flex-end"}} do
               M.view {style: M.css {flexDirection: "row"}} do
                 foldl (imageJsx $ length props.imageUrl) mempty props.imageUrl
--- src/foreign/Sound.purs
+++ src/foreign/Sound.purs
@@ -2,4 +2,8 @@ module Sound where
 import Prelude
 import Effect (Effect)
 
-foreign import play :: String -> Effect Unit
+type Sound = {}
+foreign import createSound :: String -> Sound
+foreign import play :: Sound -> Effect Unit
+foreign import stop :: Sound -> Effect Unit -> Effect Unit
+foreign import release :: Sound -> Effect Unit
