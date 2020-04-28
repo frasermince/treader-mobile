782ca163bd8cf24917f8d5ed6808f0b83b17d48d
Fraser Mince <fraser@thoughtbot.com>
2020-04-28 17:07:37 -0500

    Add left and right information to track progress.

--- src/Home.purs
+++ src/Home.purs
@@ -7,6 +7,7 @@ import React.Basic.Hooks (JSX, ReactComponent, component, element, useState, use
 import Markup as M
 import Paper (textInput, surface, button, listSection, listItem, listIcon, divider, title)
 import Effect.Uncurried (runEffectFn2, EffectFn2)
+import Record.Unsafe.Union (unsafeUnion)
 
 type Props = { navigation :: { navigate :: EffectFn2 String {} Unit } }
 
@@ -20,15 +21,18 @@ buildJsx props = React.do
   let redirectBook = runEffectFn2 props.navigation.navigate "Read" {}
   let redirectCreate = runEffectFn2 props.navigation.navigate "Create" {}
   let redirectReview = runEffectFn2 props.navigation.navigate "Review" {}
+  let ratioDone p = M.getJsx $ M.text {style: M.css {marginTop: 8}} $ M.string "0/10"
+  let checkEmptyIcon p = element listIcon $ unsafeUnion p { color: "#000", icon: "checkbox-blank-outline" }
+
   pure $ M.getJsx
     $ M.safeAreaView { style: M.css { flex: 1, backgroundColor: "#ffffff" } } do
        surface { style: M.css { flex: 1 } } do
           title { style: M.css {textAlign: "center"}} $ M.string "Daily Tasks"
           listSection {} do
-             listItem {title: RN.string "Read 10 Pages", onPress: RNE.capture_ redirectBook }
+             listItem {title: RN.string "Read 10 Pages", onPress: RNE.capture_ redirectBook, left: checkEmptyIcon, right: ratioDone}
              divider {style: M.css {height: 1, width: "100%"}}
-             listItem {title: RN.string "Create 10 Flashcards", onPress: RNE.capture_ redirectCreate }
+             listItem {title: RN.string "Create 10 Flashcards", onPress: RNE.capture_ redirectCreate, left: checkEmptyIcon, right: ratioDone }
              divider {style: M.css {height: 1, width: "100%"}}
-             listItem {title: RN.string "Review 10 Flashcards", onPress: RNE.capture_ redirectReview}
+             listItem {title: RN.string "Review 10 Flashcards", onPress: RNE.capture_ redirectReview, left: checkEmptyIcon, right: ratioDone}
              divider {style: M.css {height: 1, width: "100%"}}
 
